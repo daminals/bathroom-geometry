a11f57062d439d69f7bddc98bd5137a5b5f15971
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2024-02-05 10:37:46 -0500

    cloudflare-ify application:

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -7,6 +7,15 @@ declare global {
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
+       interface Platform {
+             env: {
+                 COUNTER: DurableObjectNamespace;
+             };
+             context: {
+                 waitUntil(promise: Promise<any>): void;
+             };
+             caches: CacheStorage & { default: Cache }
+         }
 	}
 }
 
